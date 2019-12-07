ac11286368519487190efb75ffff092729a6de92
shabari444 <38428511+shabari444@users.noreply.github.com>
2019-12-07 13:32:23 +0530

    Discovery Server Registration
    Discovery Server Added to register our services

---
+++ src/main/java/com/rest/EurekaDiscoveryServer/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.rest.EurekaDiscoveryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+	}
+
+}
