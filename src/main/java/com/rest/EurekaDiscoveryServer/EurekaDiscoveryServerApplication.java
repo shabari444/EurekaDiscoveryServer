d72d3c4f94fdfa8fc4b1d6c95e4f7d3e35b3d363
shabari444 <38428511+shabari444@users.noreply.github.com>
2019-12-21 18:11:41 +0530

    Cofig Server

--- src/main/java/com/rest/EurekaDiscoveryServer/EurekaDiscoveryServerApplication.java
+++ src/main/java/com/rest/EurekaDiscoveryServer/EurekaDiscoveryServerApplication.java
@@ -2,10 +2,12 @@ package com.rest.EurekaDiscoveryServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
